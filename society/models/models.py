8f9ca61ebd84aa9fc6cd44f69dc0f6482eb458c1
Mehdi Harmouchi <mehdi.harmouchi@gmail.com>
2022-04-08 09:40:14 +0000

    Adding departement and active field

--- society/models/models.py
+++ society/models/models.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from attr import field
 from odoo import models, fields, api
 
 
@@ -9,7 +10,10 @@ class society(models.Model):
 
     name = fields.Char('Name',required=True)
     age = fields.Integer('Age', required=True)
+    departement = fields.Char('Departement',default='Departement A')
     gender = fields.Selection(selection=[('male','Male'),('female','Female')])
     adress = fields.Text('Adress',required=True)
     salary = fields.Float('Salary',required=True)
+    
+    active = fields.Boolean('Active',default=False)
 
