407e8fdf6f1b03c91747d16acbde409abadc350a
khadija <you@example.com>
2022-04-08 22:47:01 +0000

    16:07

--- open_academy/models/models.py
+++ open_academy/models/models.py
@@ -3,10 +3,39 @@
 from odoo import models,fields
 class course (models.Model):
         _name = 'open_academy.course'
-        _description = "openAcademycoureses"
+        _description = "openAcademy coureses"
         name = fields.Char(string="title" , required=True)
         description = fields.Text()
         year= fields.Date()
+
+"""class sessionn"""
+class session(models.model):
+        _name="openacademy_sessions"
+        name=fields.char()
+        start_date=fields.date()
+        duration=fields.float(digts=(6,2), help="Duration in days")
+        seats=fields.Integer(string="Number of seats")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        #par
 #     value2 = fields.Float(compute="_value_pc", store=True)
 #     description = fields.Text()
 #
@@ -14,3 +43,4 @@ class course (models.Model):
 #     def _value_pc(self):
 #         for record in self:
 #             record.value2 = float(record.value) / 100
+
