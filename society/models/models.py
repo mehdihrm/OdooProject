854fe05e5429e439a91eace026f02d477314517e
Mehdi Harmouchi <mehdi.harmouchi@gmail.com>
2022-04-08 14:06:17 +0000

    Adding security model for product/client/society

--- society/models/models.py
+++ society/models/models.py
@@ -20,3 +20,33 @@ class society(models.Model):
     
     active = fields.Boolean('Active',default=False)
     status = fields.Selection(selection = [('new','New'),('manager','Manager'),('Chief','chief')])
+
+class society(models.Model):
+    _name = 'society.client'
+    _description = 'f_society client'
+
+    name = fields.Char('Name',required=True)
+    age = fields.Integer('Age', required=True)
+    phone_number = fields.Char('Phone Number')
+    adress = fields.Text('Adress',required=True)
+    active = fields.Boolean('Active',default=False)
+
+class society(models.Model):
+    _name = 'society'
+    _description = 'f_society client'
+
+    society_name = fields.Char('Name',required=True,readonly=True)
+    population = fields.Integer('Age', required=True,readonly=True)
+   
+
+class society(models.Model):
+    _name = 'society.product'
+    _description = 'f_society product'
+
+    name = fields.Char('Product name',required=True)
+    category = fields.Selection(selection = [('phone','Phone'),('computer','Computer'),('smarttv','SmartTv')])
+    price = fields.Float('Price',required=True)
+    active = fields.Boolean('Active',default=False)
+
+
+    
