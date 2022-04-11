64346acff083189d990d260c8bd62b4bd9763d68
Mehdi Harmouchi <mehdi.harmouchi@gmail.com>
2022-04-11 00:33:32 +0000

    more cleaner code architecture

---
+++ society/models/client.py
@@ -0,0 +1,15 @@
+from attr import field
+from odoo import models, fields, api
+
+
+
+
+class society(models.Model):
+    _name = 'society.client'
+    _description = 'f_society client'
+
+    name = fields.Char('Name',required=True)
+    birth_date = fields.Date('Date of birth',required=True)
+    phone_number = fields.Char('Phone Number')
+    adress = fields.Text('Adress',required=True)
+    active = fields.Boolean('Active',default=False)
