5e9eeba3cf70344dc0e0a3390c720d1dcfcbae40
Noura Hrida <noura01hrida@gmail.com>
2022-04-14 11:39:35 +0000

    Heritage solved

--- gstRdv/__manifest__.py
+++ gstRdv/__manifest__.py
@@ -20,13 +20,15 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base','web'],
 
     # always loaded
     'data': [
         'security/ir.model.access.csv',
         'views/views.xml',
-        'views/partial_class_partner_view.xml',
+        'views/res_users.xml',
+        'views/res_partner.xml',
+
         'data/sequenceCodes.xml',
         'views/templates.xml',
     ],
