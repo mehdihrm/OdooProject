2b0a6d9bb48ee1d653c7f565d3c198a656433320
Mehdi Harmouchi <mehdi.harmouchi@gmail.com>
2022-04-15 11:55:04 +0000

    Addings Product Manager and Product Salesman groups and specified some acces rights

--- society/__manifest__.py
+++ society/__manifest__.py
@@ -25,6 +25,7 @@
     # always loaded
     'data': [
         'security/ir.model.access.csv',
+        'security/security.xml',
         'views/views.xml',
         'views/templates.xml',
         'views/employee.xml',
@@ -32,7 +33,7 @@
         'views/product.xml',
         'views/product_tag.xml',
         'views/user_view.xml',
-        'views/account_view.xml'
+        'views/account_view.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
--- society/models/res_partner.py
+++ society/models/res_partner.py
@@ -1,3 +1,4 @@
+from pickle import FALSE
 from attr import field
 from odoo import models, fields, api
 
@@ -6,4 +7,5 @@ class Partner(models.Model):
     _inherit='account.move'
     
     client_id = fields.Many2one("society.client")
+    show_client_id = fields.Boolean(default=False)
     
